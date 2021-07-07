b80f5942d91b53f09418f2d02f00655709d35041
CristianP <cristiangiovannip@gmail.com>
2021-07-07 11:23:00 -0500

    Conexion Data Base - MySQL

--- src/Acceso/UsuarioDAO.java
+++ src/Acceso/UsuarioDAO.java
@@ -9,23 +9,21 @@ import java.util.logging.Logger;
 
 public class UsuarioDAO {
 
-    public String[] Login(UsuarioVO User) {
+    public String[] ConsultaUserPass(UsuarioVO UVO) {
         try {
-            String[] Nombre = new String[2];
+            String[] Nombre = new String[3];
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Conectando");
             Connection Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/LIBRERIA", "root", "Cazador1");
-            System.out.println("Conectado");
-            System.out.println(User.getName());
-            PreparedStatement Sentencia = Conexion.prepareStatement("SELECT Name, Pass FROM Users WHERE (Name = ?) AND (Pass = ?)");
-            Sentencia.setString(1, User.getName());
-            Sentencia.setString(2, User.getPass());
+            PreparedStatement Sentencia = Conexion.prepareStatement("SELECT Name, Pass, UserType FROM Users WHERE (Name = ?) AND (Pass = ?)");
+            Sentencia.setString(1, UVO.getName());
+            Sentencia.setString(2, UVO.getPass());
 
             ResultSet datos = Sentencia.executeQuery();
 
             while (datos.next()) {
                 Nombre[0] = datos.getString("Name");
                 Nombre[1] = datos.getString("Pass");
+                Nombre[2] = datos.getString("UserType");
             }
 
             Conexion.close();
@@ -33,6 +31,21 @@ public class UsuarioDAO {
         } catch (Exception e) {
             System.out.println("Error en UsuarioDAO: " + e);
             return null;
+        }
+    }
+
+    public void RegistroUser(UsuarioVO UVO) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/LIBRERIA", "root", "Cazador1");
+            PreparedStatement Sentencia = Conexion.prepareStatement("INSERT INTO Users (Name, Mail, Pass, UserType) VALUES (?,?,?,'Usuario');");
+            Sentencia.setString(1, UVO.getName());
+            Sentencia.setString(2, UVO.getMail());
+            Sentencia.setString(3, UVO.getPass());
+            Sentencia.executeUpdate();
+            Sentencia.close();
+        } catch (Exception e) {
+            System.out.println("Error en RegistroUser" + e);
         }
     }
 }
--- src/Negocio/DelegadoUsuarios.java
+++ src/Negocio/DelegadoUsuarios.java
@@ -3,22 +3,55 @@ package Negocio;
 import Acceso.UsuarioDAO;
 import Acceso.UsuarioVO;
 import Presentacion.Login;
+import Presentacion.SignIn;
+import javax.swing.JOptionPane;
 
 public class DelegadoUsuarios {
 
-    public void consultarUsuario(Login Login) {
+    public boolean Login(Login Login) {
 
         UsuarioVO UVO = new UsuarioVO();
         UVO.setName(Login.txtUser.getText());
         UVO.setPass(Login.txtPass.getText());
-        String[] Nombre = new UsuarioDAO().Login(UVO);
+        String[] Nombre = new UsuarioDAO().ConsultaUserPass(UVO);
 
         if (Nombre[0] == null && Nombre[1] == null) {
-            System.out.println("Usuario y/o Password incorrectos");
+            return false;
         } else {
             if (Nombre[0].equals(UVO.getName()) && Nombre[1].equals(UVO.getPass())) {
                 System.out.println("Creo q funciono");
             }
+            return true;
+        }
+    }
+    
+    public boolean SignIn(SignIn SignIn){
+        UsuarioVO UVO = new UsuarioVO();
+        UVO.setName(SignIn.txtName.getText());
+        UVO.setPass(SignIn.txtPass.getText());
+        UVO.setMail(SignIn.txtMail.getText());
+        
+        String [] Nombre = new UsuarioDAO().ConsultaUserPass(UVO);
+        
+        if (Nombre[1] == null){
+            new UsuarioDAO().RegistroUser(UVO);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean UserType(Login Login){
+        UsuarioVO UVO = new UsuarioVO();
+        UVO.setName(Login.txtUser.getText());
+        UVO.setPass(Login.txtPass.getText());
+        
+        String [] Nombre = new UsuarioDAO().ConsultaUserPass(UVO);
+        
+        if(Nombre [2].equals("Administrador")){
+            return true;
+        }else{
+            return false;
         }
     }
 }
